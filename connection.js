69babdf806fff05502a50f32db9c3d8e420915fa
makimardz <makimardz@gmail.com>
2023-06-08 14:46:25 +0800

    update

--- connection.js
+++ connection.js
@@ -1,12 +1,9 @@
 // Adding connection string into application
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require("dotenv");
+dotenv.config();
 
-/**
-* Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-* See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-*/
 const uri = "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cse341.nturncy.mongodb.net/?retryWrites=true&w=majority";
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -15,7 +12,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
