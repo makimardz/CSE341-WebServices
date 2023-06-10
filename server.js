0ae12616aa77ea0c1022b1d1eed5ca27d909bcbb
makimardz <makimardz@gmail.com>
2023-06-10 20:23:31 +0800

    L2 Personal Assignment

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connection');
+
+const port = process.env.PORT || 3000;
 const app = express();
 
-// Define a route that returns a name
-app.get('/', (req, res) => {
-  const name = 'Mardz Izech'; // return with the name of someone I know
-  res.send(name);
-});
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-// Start the server
-const port = 3000; // You can change the port number if needed
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
